51687b3225222e1bd09cae8df864260a4a1ef76a
liptz <40310207+liptz@users.noreply.github.com>
2019-05-09 20:27:15 +0900

    Update KeInterface.h

--- ApexLegends/KeInterface.h
+++ ApexLegends/KeInterface.h
@@ -1,6 +1,4 @@
 #include <Windows.h>
-#include <TlHelp32.h>
-#include <iostream>
 using namespace std;
 
 #define IO_READ_REQUEST CTL_CODE(FILE_DEVICE_UNKNOWN, 0x0701, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
@@ -32,58 +30,15 @@ typedef struct _KERNEL_GET_MODULE_REQUEST
 	PVOID Address;
 } KERNEL_GET_MODULE_REQUEST, *PKERNEL_GET_MODULE_REQUEST;
 
-typedef struct _Vector3
-{
-	float x;
-	float y;
-	float z;
-} Vector3;
-
-typedef struct _RGB
-{
-	float x;
-	float y;
-	float z;
-} RGB;
-
-DWORD GetPId(string processName)
-{
-	PROCESSENTRY32 processInfo;
-	HANDLE processSnapshot;
-	processInfo.dwSize = sizeof(processInfo);
-
-	processSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
-	if (processSnapshot == INVALID_HANDLE_VALUE)
-		return 0;
-
-	Process32First(processSnapshot, &processInfo);
-	if (!processName.compare(processInfo.szExeFile))
-	{
-		CloseHandle(processSnapshot);
-		return processInfo.th32ProcessID;
-	}
-
-	while (Process32Next(processSnapshot, &processInfo))
-	{
-		if (!processName.compare(processInfo.szExeFile))
-		{
-			CloseHandle(processSnapshot);
-			return processInfo.th32ProcessID;
-		}
-	}
-
-	CloseHandle(processSnapshot);
-	return 0;
-}
-
 class KeInterface
 {
-public:
+private:
 	HANDLE hDriver;
-	KeInterface(LPCSTR RegistryPath)
-	{
-		hDriver = CreateFileA(RegistryPath, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_EXISTING, 0, 0);
-	}
+public:
+	KeInterface(LPCSTR RegistryPath);
+	BOOL IsDriver();
+	PVOID GetClientModule(DWORD processId);
+	BOOL ClearUnloadedDriver();
 
 	template <typename T>
 	T ReadVirtualMemory(ULONG ProcessId, DWORD64 ReadAddress)
@@ -102,7 +57,7 @@ public:
 
 		return FALSE;
 	}
-
+	
 	template <typename T>
 	BOOL WriteVirtualMemory(ULONG ProcessId, DWORD64 WriteAddress, T WriteValue)
 	{
@@ -117,31 +72,6 @@ public:
 		WriteRequest.Size = sizeof(WriteValue);
 
 		if (DeviceIoControl(hDriver, IO_WRITE_REQUEST, &WriteRequest, sizeof(WriteRequest), 0, 0, 0, 0))
-			return TRUE;
-
-		return FALSE;
-	}
-
-	PVOID GetClientModule(DWORD processId)
-	{
-		if (hDriver == INVALID_HANDLE_VALUE)
-			return false;
-
-		KERNEL_GET_MODULE_REQUEST moduleRequest;
-		moduleRequest.ProcessId = processId;
-
-		if (DeviceIoControl(hDriver, IO_GET_MODULE_REQUEST, &moduleRequest, sizeof(moduleRequest), &moduleRequest, sizeof(moduleRequest), 0, 0))
-			return moduleRequest.Address;
-
-		return false;
-	}
-
-	BOOL ClearUnloadedDriver()
-	{
-		if (hDriver == INVALID_HANDLE_VALUE)
-			return FALSE;
-
-		if (DeviceIoControl(hDriver, IO_CLEAR_UNLOADEDDRIVER_REQUEST, NULL, NULL, NULL, NULL, NULL, NULL))
 			return TRUE;
 
 		return FALSE;
