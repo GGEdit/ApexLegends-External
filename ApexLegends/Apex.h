bc7218f221dbba9a228ca67d7f268319fc37e847
AAALover <40310207+AAALover@users.noreply.github.com>
2019-09-11 01:27:07 +0900

    Add files via upload

---
+++ ApexLegends/Apex.h
@@ -0,0 +1,15 @@
+#include "KeInterface.h"
+#include "Process.h"
+#include "Offset.h"
+#include "Loot.h"
+#include "Glow.h"
+#include "Weapon.h"
+
+KeInterface Driver;
+
+typedef struct _Vector3
+{
+	float X;
+	float Y;
+	float Z;
+} Vector3;
